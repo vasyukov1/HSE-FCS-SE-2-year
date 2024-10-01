e1d2109bc6265deddc46e8720273651dd98e56e6
Alexander Vasyukov <vasyukov2005al@yandex.ru>
2024-10-01 12:56:56 +0300

    Added report for ACS hw 4

--- ACS/Homework/4/solution.asm
+++ ACS/Homework/4/solution.asm
@@ -2,16 +2,12 @@
 n: 		.word 0
 array:		.space 40
 
-upper_limiter:	.word 2147483647
-lower_limiter:	.word -2147483648
-
-new_line:	.asciz "\n"
 input_size:	.asciz "Input the size of array from 1 to 10: "
 input_item_p1:	.asciz "Input "
 input_item_p2:	.asciz " element: "
-result_prompt:	.asciz "Sum: "
-error_prompt:	.asciz "Bro, you need input a number from 1 to 10. Try again!\n"
-overflow_p1:	.asciz "Overflow!\nSum: "
+result_prompt:	.asciz "\nSum: "
+error_prompt:	.asciz "\nBro, you need input a number from 1 to 10. Try again!\n"
+overflow_p1:	.asciz "\nOverflow!\nSum: "
 overflow_p2:	.asciz "\nCount of elements: "
 
 
@@ -41,7 +37,6 @@ size:	# Output prompt for input the size of array
 	
 	li t1 1
 	lw t3 n
-
 	
 fill:	# Prompt for input the element of array
 	li a7 4
@@ -60,28 +55,25 @@ fill:	# Prompt for input the element of array
 	ecall
 	mv t2 a0
 	
+	# Save element and go to the next
 	sw t2 (t0)
 	addi t0 t0 4
-	
 	addi t1 t1 1
 	ble t1 t3 fill
 
 sum_preparation:	
-	la t0 array		# Start of array
-	li t1 0			# Sum of array
+	la t0 array	# Start of array
+	li t1 0		# Sum of array
 	li t2 0		# Counter of elements
-	lw t3 n			# Size of array
+	lw t3 n		# Size of array
 	
-add_el:	lw t4 (t0)		# Read element
-
+add_el:	lw t4 (t0)	# Read element
 	# Check overflow
 	xor t5 t1 t4
-	bgez t5 check_overflow
-	
+	bgez t5 check_overflow	
 no_overflow:
 	# Save sum
 	add t1 t1 t4		
-
 	# Go to next element
 	addi t0 t0 4
 	addi t2 t2 1		
@@ -89,12 +81,14 @@ no_overflow:
 	j result
 	
 check_overflow:
+	# If sum and term have different signs, overflow occurred
 	add t6 t1 t4
 	xor t5 t1 t6
 	bltz t5 error_overflow
 	j no_overflow	
 	
 result:
+	# Output result 
 	li a7 4
 	la a0 result_prompt
 	ecall
@@ -131,8 +125,6 @@ error:
 	ecall
 	j size
 	
-end:
+end:	# Stop
 	li a7 10
 	ecall
-	
-	
